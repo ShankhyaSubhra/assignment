1d5884f3f1576bd0917be34abfd39b2abf7a1f8c
Shankhya Subhra Datta <shankhyasubhradatta@gmail.com>
2022-02-24 17:53:30 +0530

    added payment method

--- src/controller/payment.js
+++ src/controller/payment.js
@@ -1,4 +1,6 @@
 const Razorpay = require('razorpay')
+const axios = require('axios')
+const crypto = require('crypto');
 
 let instance = new Razorpay({
     key_id: process.env.RZR_KEY_ID,
@@ -7,22 +9,48 @@ let instance = new Razorpay({
 
 // Route for creating order
 const createOrder = async (req, res) => {
-    let options = {
-        amount: req.body.amount,  // amount in the smallest currency unit
-        currency: "INR",
-        receipt: "order_rcptid_11"
-    };
-      
-    instance.orders.create(options, function(err, order) {
-        if (err) {
-            res.send(err)
-        }
-        res.send(order)
-        // console.log(order);
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic cnpwX3Rlc3RfZzQ0YTVha0JiSDF2bG46OThiOThzcmtySjNHQThheXFoSWhVemdt'
+    }
+
+    axios.post('https://api.razorpay.com/v1/orders', {
+        "amount": req.body.amount,
+        "currency": "INR",
+        "receipt": req.body.receipt
+    },
+    {
+        headers: headers
+    })
+    .then(function (response) {
+        console.log(response.data)
+        res.send(response.data)
+    })
+    .catch(function (error) {
+        res.send(error)
     });
+}
+
+// Route for verifying payment signature
+const verifyPaymentSignature = async (req, res) => {
+    try {
+        let body = req.body.order_id + "|" + req.body.razorpay_payment_id
+    
+        let generated_signature = crypto.createHmac('sha256', process.env.RZR_KEY_SECRECT).update(body.toString()).digest('hex')
+        
+        if (generated_signature == req.body.razorpay_signature) {
+            res.send('Payment successful')
+        } else {
+            res.send('Payment unsuccessful')
+        }
+    } catch (e) {
+        res.send(e)
+    }
+    
 }
 
 // Expoting controller functions
 module.exports = {
-    createOrder
+    createOrder,
+    verifyPaymentSignature
 }
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -8,5 +8,8 @@ const router = new express.Router()
 // Route for user signup
 router.post('/createorder', paymentController.createOrder)
 
+//Route for verifying payment signature
+router.post('/verifypaymentsignature', paymentController.verifyPaymentSignature)
+
 // Exporting express router
 module.exports = router
